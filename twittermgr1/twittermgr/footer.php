bf6e81409ec12e3e9095bcc24c25789c50226cdd
imtiaz818 <imtiaz_ahmad008@hotmail.com>
2013-08-03 12:57:47 +0100

    25% work done by till this point

---
+++ twittermgr1/twittermgr/footer.php
@@ -0,0 +1,58 @@
+<div id="home" class="content">
+            <div class="white panel shadowed">
+                <div class="container">
+                    <div class="twoColumn howitworks">
+                        <div class="column text">
+                            <h2>How does it work?</h2>
+                            <p>Create an account and we'll pick up and digitize your postal mail three times a week.</p>
+                            <p>View your mail anywhere, anytime.</p>
+                            <p>Request any mail item and we'll deliver it to you.</p>
+                            <a href="signin.php">Learn more</a>
+                        </div>
+                        <div class="column">
+                            <div class="img"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer">
+                <div class="container">
+                    <div class="content">
+                        <div class="links">
+                            <div class="column">
+                                <h5>Policies</h5>
+                                <a href="privacy">Privacy Policy</a>
+                                <a href="terms">Terms of Use</a>
+                            </div>
+                            <div class="column">
+                                <h5>About Us</h5>
+                                <a href="team">The Team</a>
+                                <a href="blogs" style="display:none;">Blogs</a>
+                                <a href="careers">Careers</a>
+                                <a href="press">Press</a>
+                            </div>
+                            <div class="column">
+                                <h5>Connect</h5>
+                                <a href="" target="_blank">Our Blog</a>
+                                <a href="" target="_blank">Facebook</a>
+                                <a href="" target="_blank">Twitter</a>
+                                <a href="">Email</a>
+                            </div>
+                            <div class="copyright">
+                                Copyright &copy; 2013 writething. All rights reserved.
+                            </div>
+                        </div>
+                        <div class="seenon">
+                            <div class="banner"></div>
+                            <div class="iconlinks">
+                                <a class="col1 cnbc"></a>
+                                <a class="col2 keye"></a>
+                                <br>
+                                <a class="col1 inc"></a>
+                                <a class="col2 univision"></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
